680f5aa6ba9e702d6eea6e1db0f8b3c43354c52a
San Ayush <41052723+San1357@users.noreply.github.com>
2021-05-01 18:27:14 +0530

    Create Prefix_and_Suffix_Search.py

---
+++ Prefix_and_Suffix_Search.py
@@ -0,0 +1,14 @@
+'''Problem : PREFIX AND SUFFIX SEARCH '''
+
+#CODE :
+
+
+class WordFilter:
+    def __init__(self, words):
+        self.d = {}
+        for i, word in enumerate(words):
+            for p, s in product(range(len(word) + 1), repeat=2):
+                self.d[word[:p], word[s:]] = i
+
+    def f(self, prefix, suffix):
+        return self.d.get((prefix, suffix), -1)
