6c50b6c86fe51429891981ea37270a51f913990f
San Ayush <41052723+San1357@users.noreply.github.com>
2021-05-10 11:35:43 +0530

    Create Construct_Target_Array_With_Multiple_Sums.py

---
+++ Construct_Target_Array_With_Multiple_Sums.py
@@ -0,0 +1,17 @@
+'''PRoblem :  Construct Target Array With Multiple Sums '''
+
+#CODE :
+
+class Solution:
+    def isPossible(self, target: List[int]) -> bool:
+        heap = [-num for num in target]
+        total = sum(target)
+        heapify(heap)
+        while heap[0] != -1:
+            num = -heappop(heap)
+            total -= num
+            if num <= total or total < 1: return False
+            num %= total
+            total += num
+            heappush(heap, -num or -total)
+        return True
