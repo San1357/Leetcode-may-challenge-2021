4475d5f25d185c4b72a224f51c66b4ce9ff1c001
San Ayush <41052723+San1357@users.noreply.github.com>
2021-05-12 11:43:53 +0530

    Create Maximum_Points_You_Can_Obtain_from_Cards.py

---
+++ Maximum_Points_You_Can_Obtain_from_Cards.py
@@ -0,0 +1,11 @@
+''' Problem : Maximum Points You Can Obtain from Cards  '''
+
+#CODE :
+
+class Solution:
+    def maxScore(self, C: List[int], K: int) -> int:
+        best = total = sum(C[:K])
+        for i in range (K-1, -1, -1):
+            total += C[i + len(C) - K] - C[i]
+            best = max(best, total)
+        return best
